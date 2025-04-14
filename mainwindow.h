855fa1fe6a8924be28b90deb662a866fd8ad58eb
Sam <3237003356@qq.com>
2025-04-14 17:26:40 +0800

    新增了途经点功能

--- mainwindow.h
+++ mainwindow.h
@@ -47,6 +47,7 @@ private:
     RoutePlanner *planner;    // 路径规划器
     int startPoint = -1;  // 起点
     int endPoint = -1;    // 终点
+    std::vector<int> viaPoints;
 
     QGraphicsItemGroup *currentPathItemGroup;  // 存储当前高亮路径的图形项组
     void setupUI();  // 设置界面
